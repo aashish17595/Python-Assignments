b6a70571f1a36e0c55c1dcbbcc9c18090af91aea
aashish17595 <164724762+aashish17595@users.noreply.github.com>
2025-04-29 20:16:35 +0530

    Assignment 1 from Module 2 basic python projects

---
+++ Task 1.py
@@ -0,0 +1,12 @@
+a=input("Enter the first number : ")
+b=input("Enter the second number : ")
+a=float(a)
+b=float(b)
+add=a + b  #add ----> addition
+sub=a - b  #sub ----> subtraction
+mul=a * b  #mul ----> multiplication
+div=a / b  #div ----> division
+print("Addition : ",add)
+print("Subtraction : ",sub)
+print("Multiplication : ",mul)
+print("Division : ",div)
